cdbd8052f578ecf4ae9666079da1bb056ea6912e
praneeth622 <praneethdevarasetty31@gmail.com>
2025-01-23 18:49:37 +0530

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import TiptapEditor from "@/components/tiptap-editor"
+
+export default function Page() {
+  return (
+    <main className="min-h-screen p-4 bg-gray-50">
+      <TiptapEditor />
+    </main>
+  )
+}
+
